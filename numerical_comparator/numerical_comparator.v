dc6b3044110680a3bfcb9d91b08884b0c811ccdf
HRL <1684525025@qq.com>
2020-12-26 20:20:27 +0800

    some modification on numerical_comparator

--- numerical_comparator/numerical_comparator.v
+++ numerical_comparator/numerical_comparator.v
@@ -1,11 +1,23 @@
 module numerical_comparator(A,B,Fmax,Fmin,Fequ);
-	input A,B;
+	input [3:0] A,B;
 	output Fmax,Fmin,Fequ;
-	wire 	Anot,Bnot;
 	
-	not U1(Bnot,B);
-	not U2(Anot,A);
-	and U3(Fmax,A,Bnot);
-	and U4(Fmin,B,Anot);
-	nor U5(Fequ,Fmax,Fmin);
+	wire Fmax3,Fmin3,Fequ3,Fmax2,Fmin2,Fequ2,Fmax1,Fmin1,Fequ1,Fmax0,Fmin0,Fequ0;
+	
+	assign Fmax3 = A[3]&(~B[3]);
+	assign Fmin3 = (~A[3])&B[3];
+	assign Fequ3 = A[3]&B[3] | (~A[3])&(~B[3]);
+	assign Fmax2 = A[2]&(~B[2]);
+	assign Fmin2 = (~A[2])&B[2];
+	assign Fequ2 = A[2]&B[2] | (~A[2])&(~B[2]);
+	assign Fmax1 = A[1]&(~B[1]);
+	assign Fmin1 = (~A[1])&B[1];
+	assign Fequ1 = A[1]&B[1] | (~A[1])&(~B[1]);
+	assign Fmax0 = A[0]&(~B[0]);
+	assign Fmin0 = (~A[0])&B[0];
+	assign Fequ0 = A[0]&B[0] | (~A[0])&(~B[0]);
+	assign Fmax = Fmax3 | Fequ3&Fmax2 | Fequ3&Fequ2&Fmax1 | Fequ3&Fequ2&Fequ1&Fmax0;
+	assign Fmin = Fmin3 | Fequ3&Fmin2 | Fequ3&Fequ2&Fmin1 | Fequ3&Fequ2&Fequ1&Fmin0;
+	assign Fequ = Fequ3&Fequ2&Fequ1&Fequ0;
+	
 endmodule
--- numerical_comparator/simulation/qsim/numerical_comparator.do
+++ numerical_comparator/simulation/qsim/numerical_comparator.do
@@ -1,7 +1,7 @@
 onerror {exit -code 1}
 vlib work
 vlog -work work numerical_comparator.vo
-vlog -work work Waveform.vwf.vt
+vlog -work work Waveform2.vwf.vt
 vsim -novopt -c -t 1ps -L maxv_ver -L altera_ver -L altera_mf_ver -L 220model_ver -L sgate_ver -L altera_lnsim_ver work.numerical_comparator_vlg_vec_tst
 vcd file -direction numerical_comparator.msim.vcd
 vcd add -internal numerical_comparator_vlg_vec_tst/*
